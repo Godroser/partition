4c15e45a50952a4b328c803d4b37d32fef049228
godroser <dzhdxy@163.com>
2024-10-02 10:37:37 +0800

    add func

--- dev/connect.py
+++ dev/connect.py
@@ -33,7 +33,7 @@ def get_connection(autocommit: bool = True) -> MySQLConnection:
         db_conf["ssl_ca"] = ca_path
     return mysql.connector.connect(**db_conf)
 
-def create_table() -> None:
+def create_table_hash() -> None:
     with get_connection(autocommit=False) as connection:
         with connection.cursor() as cur:
             cur.execute("DROP TABLE IF EXISTS players;")
@@ -52,27 +52,124 @@ def create_table() -> None:
             )
 
 
+def create_table_range():
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur:
+            cur.execute("DROP TABLE IF EXISTS players;")
+            cur.execute(
+                """
+                CREATE TABLE players (
+                    `id` INTEGER,
+                    `coins` INTEGER,
+                    `goods` INTEGER, PRIMARY KEY (`id`)
+                ) 
+                
+                PARTITION BY RANGE(id) (
+                    PARTITION p0 VALUES LESS THAN (500),
+                    PARTITION p1 VALUES LESS THAN (1000),
+                    PARTITION p2 VALUES LESS THAN (1500),
+                    PARTITION p3 VALUES LESS THAN (2000),
+                    PARTITION p4 VALUES LESS THAN (2500),
+                    PARTITION p5 VALUES LESS THAN (3000),
+                    PARTITION p6 VALUES LESS THAN (4000),
+                    PARTITION p7 VALUES LESS THAN (5000)    
+                );
+
+                """
+            )    
+
 def insert_table(N) -> None:
     for i in range(N):
-        random_coins = random.randint(1, 1000) 
-        random_goods = random.randint(1, 1000) 
+        random_coins = random.randint(1, 10000) 
+        random_goods = random.randint(1, 10000) 
         player = (i, random_coins, random_goods)
         with get_connection(autocommit=True) as connection:
             with connection.cursor() as cur:
                 cur.execute("INSERT INTO players (id, coins, goods) VALUES (%s, %s, %s)", player)    
 
 
+def repartition_range_to_hash() -> None:
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur: 
+            cur.execute(
+                """
+                CREATE TABLE new_players (
+                    `id` INTEGER,
+                    `coins` INTEGER,
+                    `goods` INTEGER, PRIMARY KEY (`id`)
+                )
+
+                PARTITION BY HASH(id)
+                PARTITIONS 8;
+                """
+            )
+
+            cur.execute("INSERT INTO new_players SELECT * FROM players;")
+
+            cur.execute("DROP TABLE players;")
+
+            cur.execute("ALTER TABLE new_players RENAME TO players;")
+                        
+
+def repartition_hash_to_range() -> None:
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur: 
+            cur.execute(
+                """
+                CREATE TABLE new_players (
+                    `id` INTEGER,
+                    `coins` INTEGER,
+                    `goods` INTEGER, PRIMARY KEY (`id`)
+                ) 
+                
+                PARTITION BY RANGE(id) (
+                    PARTITION p0 VALUES LESS THAN (5000),
+                    PARTITION p1 VALUES LESS THAN (10000),
+                    PARTITION p2 VALUES LESS THAN (15000),
+                    PARTITION p3 VALUES LESS THAN (20000),
+                    PARTITION p4 VALUES LESS THAN (25000),
+                    PARTITION p5 VALUES LESS THAN (30000),
+                    PARTITION p6 VALUES LESS THAN (40000),
+                    PARTITION p7 VALUES LESS THAN (50000)    
+                );
+                """
+            )
+
+            cur.execute("INSERT INTO new_players SELECT * FROM players;")
+
+            cur.execute("DROP TABLE players;")
+
+            cur.execute("ALTER TABLE new_players RENAME TO players;")
+
+
+def add_tiflash_replica():
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur: 
+            cur.execute(    
+                "ALTER TABLE players SET TIFLASH REPLICA 1;"
+            )
+
+def check_replica_status():
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur: 
+            cur.execute(    
+                "SELECT * FROM information_schema.tiflash_replica WHERE TABLE_SCHEMA = 'tpch' and TABLE_NAME = 'players';"
+            )    
+            print(cur.fetchall())
+
 
 
 def db_exec() -> tuple:
     with get_connection(autocommit=True) as connection:
         with connection.cursor() as cur:
-            #cur.execute("explain analyze select * from players where id < 500;")
-            cur.execute(
-                """
-                SELECT      TABLE_NAME,     PARTITION_NAME,     PARTITION_ORDINAL_POSITION,     PARTITION_METHOD,     SUBPARTITION_METHOD,     PARTITION_EXPRESSION,     SUBPARTITION_EXPRESSION,     PARTITION_DESCRIPTION FROM      INFORMATION_SCHEMA.PARTITIONS WHERE      TABLE_NAME = 'players';
-                """
-            )
+            cur.execute("explain analyze select * from players where id < 20000;")
+            
+            # cur.execute(
+            #     """
+            #     SELECT      TABLE_NAME,     PARTITION_NAME,     PARTITION_ORDINAL_POSITION,     PARTITION_METHOD,     SUBPARTITION_METHOD,     PARTITION_EXPRESSION,     SUBPARTITION_EXPRESSION,     PARTITION_DESCRIPTION FROM      INFORMATION_SCHEMA.PARTITIONS WHERE      TABLE_NAME = 'players';
+            #     """
+            # )
+            
             tables = cur.fetchall()  # 处理所有结果
             print(tables)
             print(type(tables))
@@ -83,5 +180,11 @@ def db_exec() -> tuple:
 
 if __name__ == "__main__":
     db_exec()
-    #create_table()
-    #insert_table(5000)
+    #create_table_range()
+    #create_table_hash()
+    #insert_table(50000)
+    #repartition()
+    #repartition_range_to_hash()
+    #repartition_hash_to_range()
+    #add_tiflash_replica("players")
+    #check_replica_status()
